97e2e9195cf8092dad0fb28575c0642c6d101718
Pamela Yang <PamelaYang@Pamelas-MacBook-Pro.local>
2016-03-31 10:12:00 -1000

    follows taylors solution for recursion

--- public/app.js
+++ public/app.js
@@ -1,8 +1,34 @@
-function chirp() {
+// function chirp(n) {
+
+//   var myArr = [];
+//   var chirpy = ('chirp' + ' ');
+
+// //basecase
+// if(myArr.length === n){
+//   return n;
+// }
+
+//   if(myArr.length < n) {
+
+//   myArr.push(chirpy);
+//   console.log(myArr);
+//   chirp(n);
+//   }
 
 
+//   return chirpy.concat(myArr);
 
+// }
+// chirp(3);
+
+//WRONG SOLUTION ABOVE
+
+function chirp(n) {
+
+  if(n<=1){
+    return 'chirp';
+  }
+  return 'chirp ' + chirp(n-1);
 }
-
 
 module.exports = chirp;
--- test/chirp.spec.js
+++ test/chirp.spec.js
@@ -8,4 +8,12 @@ describe('chirp function', function () {
   it('should be a function', function() {
     expect(chirp).to.be.a('function');
   });
+
+  it('should return a string with the word chirp', function() {
+    expect(chirp(1)).to.be.equal('chirp');
+  });
+
+  it('should return the string chirp the number of times based on the number passed in', function() {
+    expect(chirp(3)).to.be.equal("chirp chirp chirp");
+  });
 });
